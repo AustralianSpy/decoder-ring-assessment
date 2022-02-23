983ba5a73ba49f2000e58a2ac74a6e9dbefc0aa1
Sydney Workman <sworkman1996@gmail.com>
2022-02-22 18:16:40 -0600

    helper function and tests

--- src/caesar.js
+++ src/caesar.js
@@ -4,15 +4,50 @@
 // of the anonymous function on line 6
 
 const caesarModule = (function () {
-  // you can add any code you want within this function scope
+  
+  function shiftCheck(num) {
+    // check validity of shift value; return false if it meets
+    // invalidating criteria; otherwise return true.
+    const result = !(!num || num === 0 || num > 25 || num < -25);
+    console.log(result);
+    return result;
+  }
 
   function caesar(input, shift, encode = true) {
-    // your solution code here
+    try {
+      if (!shiftCheck(shift)) throw `${shift} is not a valid number.`;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    // lowercase values are at ascii 97-122;
+    const lowerInput = input.toLowerCase();
+    let result = '';
+
+    for (let i = 0; i < lowerInput.length; i++){
+      let char = lowerInput[i];
+      let code = lowerInput.charCodeAt(i);
+      if (code > 122 || code < 97) result += char;
+      if (input > 0) {
+        code += shift;
+        (code > 122) ?
+          char = String.fromCharCode(code - 26) :
+          char = String.fromCharCode(code);
+      } else {
+        (code < 97) ?
+          char = String.fromCharCode(code + 26) :
+          char = String.fromCharCode(code);
+      }
+      result += char;
+    }
+    
+    console.log(result, `Your message has been ${(encode) ? 'encoded' : 'decoded'}!`);
+    return result;
   }
 
   return {
-    caesar,
+    caesar, shiftCheck,
   };
 })();
 
-module.exports = { caesar: caesarModule.caesar };
+module.exports = { caesar: caesarModule.caesar, shiftCheck: caesarModule.shiftCheck };
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,75 @@
-// Write your tests here!
+const { expect, assert } = require('chai');
+const { caesar, shiftCheck } = require('../src/caesar');
+
+describe('shiftCheck', () => {
+    describe('handle errors in shift values', () => {
+        it('should return false if an input number is 0', () => {
+            const actual = shiftCheck(0);
+            expect(actual).to.be.false;
+        });
+        it('should return false if an input number is greater than 25', () => {
+            const actual = shiftCheck(26);
+            expect(actual).to.be.false;
+        });
+        it('should return false if an input number is less than -25', () => {
+            const actual = shiftCheck(-26);
+            expect(actual).to.be.false;
+        });
+        it('should return true for a valid input', () => {
+            const actual = shiftCheck(7);
+            expect(actual).to.be.true;
+        });
+    });
+});
+
+describe('caesar', () => {
+    describe('handling of input errors', () => {
+        it ('should return false if an invalid shift-value is given', () => {
+            const actual = caesar('abscdefg', 0);
+            expect(actual).to.be.false;
+        });
+        it ('should return false if no input string is given', () => {
+            const actual = caesar('', 3);
+            expect(actual).to.be.false;
+        });
+    });
+    describe('outputs a properly-shifted string according to input', () => {
+        it('should return a string of equivalent length to the given input', () => {
+            const str = 'abcdefg:hello.'
+            const actual = caesar(str, 16);
+            expect(actual).to.have.a.lengthOf(14);
+        });
+        it('should treat capital letters as lowercase', () => {
+            const str = 'ABCD Hello';
+            const actual = caesar(str, 1);
+            const expected = 'bcde ifmmp';
+            expect(actual).to.equal(expected);
+        });
+        it('should ignore all nonalphabetic characters', () => {
+            const str = 'a:b!c,d? hello.'
+            const actual = caesar(str, 2);
+            const expected = 'c:d!e,f? jgnnq';
+            expect(actual).to.equal(expected);
+        });
+        it('should shift characters to the left if a negative input is given', () => {
+            const str = 'efghlmnop';
+            const actual = caesar(str, -2);
+            const expected = 'cdefjklmn';
+            expect(actual).to.equal(expected);
+        });
+    });
+    describe('it should encode and decode according to the boolean value', () => {
+        it('should encode the message according to the shift-value if encode is set to true', () => {
+            const str = 'abcdefg';
+            const actual = caesar(str, 2, true);
+            const expected = 'cdefghi';
+            expect(actual).to.equal(expected);
+        });
+        it('should decode the message according to a reversed shift-value if encode is set to false', () => {
+            const str = 'cdefghi';
+            const actual = caesar(str, 2, false);
+            const expected = 'abcdefg';
+            expect(actual).to.equal(expected);
+        });
+    })
+});
