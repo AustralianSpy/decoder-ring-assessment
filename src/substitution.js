f92b3824efeee8db98692adec1aa4b733e654981
Sydney Workman <sworkman1996@gmail.com>
2022-02-24 18:52:41 -0600

    substitution tests, error-handling, encoding

--- src/substitution.js
+++ src/substitution.js
@@ -1,18 +1,71 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const substitutionModule = (function () {
-  // you can add any code you want within this function scope
+
+  // helper function to check for duplicate characters in
+  // inputted alphabet.
+  function checkDuplicates (alphabet){
+    let str = '';
+    let areDuplicates = false;
+
+    for (let i = 0; i < alphabet.length; i++){
+      const char = alphabet[i];
+      if (str.includes(char)){
+        areDuplicates = !areDuplicates;
+        break;
+      } else {
+        str += char;
+      }
+    }
+    return areDuplicates;
+  }
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    try {
+      if (!input) throw `Please enter a message to cipher.`;
+      if (!alphabet) throw `Please enter an alphabet to reference.`;
+      if (alphabet.length !== 26) throw `Please enter a 26-character alphabet.`;
+      if (checkDuplicates(alphabet)) throw `Alphabet cannot have duplicate characters.`;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+    const standard = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    const cipher = alphabet.split('');
+    let result = '';
+    input = input.toLowerCase();
+
+    if (encode) {
+      for (let i = 0; i < input.length; i++){
+        let char = input[i];
+        if (!/[a-z]/g.test(char)){     // preserve special characters.
+          result += char;
+        } else {
+          const index = standard.findIndex(letter => letter === char);
+          char = cipher[index];
+          result += char;
+        }
+      }
+    } 
+    
+    if (!encode) {
+      for (let i = 0; i < input.length; i++){
+        let char = input[i];
+        if (char === ' '){     // preserve spaces.
+          result += char;
+        } else {
+          const index = cipher.findIndex(letter => letter === char);
+          char = standard[index];
+          result += char;
+        }
+      }
+    }
+
+    console.log(result, `— Your message has been ${(encode) ? 'encoded' : 'decoded'}!`);
+    return result;
   }
 
   return {
-    substitution,
+    substitution, checkDuplicates,
   };
 })();
 
-module.exports = { substitution: substitutionModule.substitution };
+module.exports = { substitution: substitutionModule.substitution, checkDuplicates: substitutionModule.checkDuplicates };
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,79 @@
-// Write your tests here!
+const { expect } = require('chai');
+const { substitution, checkDuplicates } = require('../src/substitution');
+
+describe('substitution', () => {
+    describe('checkDuplicates helper function', () => {
+        it('should return false if an inputted alphabet does not contain duplicates', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = checkDuplicates(alph);
+            expect(actual).to.be.false;
+        });
+        it('should return true if an inputted alphabet does contain duplicates', () => {
+            const alph = 'gjcfnkajjuoqymmietrpzsshbd';
+            const actual = checkDuplicates(alph);
+            expect(actual).to.be.true;
+        });
+    });
+    describe('alphabet + input error-handling', () => {
+        it('should return false if no input is given', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('', alph);
+            expect(actual).to.be.false;
+        });
+        it('should return false if no alphabet is given', () => {
+            const alph = '';
+            const actual = substitution('abcdhello', alph);
+            expect(actual).to.be.false;
+        });
+        it('should return false if the alphabet is not exactly 26 characters', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbdg';
+            const actual = substitution('abcdhello', alph);
+            expect(actual).to.be.false;
+        });
+        it('should return false if the alphlabet contains duplicate letters', () => {
+            const alph = 'gxcffkajvuoqywmmetrpzlssbd'
+            const actual = substitution('abcdhello', alph);
+            expect(actual).to.be.false;
+        });
+    });
+    describe('when encoding, returns a string according to the input', () => {
+        it('should encode the given message by substituting same-index characters from the given alphabet', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('abcdhello', alph);
+            const expected = 'gxcfjnqqm';
+            expect(actual).to.equal(expected);
+        });
+        it('should treat capital letters as lowercase', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('ABCDHello', alph);
+            const expected = 'gxcfjnqqm';
+            expect(actual).to.equal(expected);
+        });
+        it('should preserve all nonalphabetic characters', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('abcd hello', alph);
+            const expected = 'gxcf jnqqm';
+            expect(actual).to.equal(expected);
+        });
+    });
+    describe('when decoding, returns a string according to the input', () => {
+        it('should decode the given message by substituting same-index characters from the given alphabet', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('gxcfjnqqm', alph, false);
+            const expected = 'abcdhello';
+            expect(actual).to.equal(expected);
+        });
+        it('should treat capital letters as lowercase', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('ABCDHello', alph, false);
+            const expected = 'gxcfjnqqm';
+            expect(actual).to.equal(expected);
+        });
+        it('should preserve all nonalphabetic characters', () => {
+            const alph = 'gxcfnkajvuoqywmietrpzlshbd';
+            const actual = substitution('gxcf jnqqm', alph, false);
+            const expected = 'abcd hello';
+            expect(actual).to.equal(expected);
+        });
+    });
+});
